a042a6e1f92c824cf65f78a3c42a093a1f20b4fd
KabeloKenzo <oneelementdesign@gmail.com>
2021-12-29 10:30:26 +0200

    implementations

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -77,14 +77,14 @@ const calculateNext = (state) => {
 const iterate = (state, iterations) => {
   const states = [state];
   for (let i = 0; i < iterations; i++) {
-    states.push(calculateNext(states[states.length - i]));
+    states.push(calculateNext(states[states.length - 1]));
   }
   return states;
 };
 
 const main = (pattern, iterations) => {
   const results = iterate(startPatterns[pattern], iterations);
-  results.forEach(r => console.log(printCells(r)));
+  results.forEach(r => console.log(printCells(r)));  
 };
 
 const startPatterns = {
